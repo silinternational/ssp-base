4308e201f9298b397eb0973e9794879d896b10b0
Baggerone <baggerone@gmail.com>
2017-05-08 15:16:27 -0400

    added idp list entry

--- development/hub/metadata/sp-remote.php
+++ development/hub/metadata/sp-remote.php
@@ -10,6 +10,9 @@ return [
      * Example SimpleSAMLphp SAML 2.0 SP
      */
     'http://ssp-hub-sp.local' => [
+        'IDPList' => ['http://ssp-hub-idp.local:8085',
+                      'http://ssp-hub-idp2.local:8086'
+                     ],
         'AssertionConsumerService' => 'http://ssp-hub-sp.local:8081/module.php/saml/sp/saml2-acs.php/ssp-hub',
         'SingleLogoutService' => 'http://ssp-hub-sp.local:8081/module.php/saml/sp/saml2-logout.php/ssp-hub',
         'SkipTests' => True,
