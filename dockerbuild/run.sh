475a3e951550ed98524c1cffb943f5efa68552c8
Steven Schram <steve_schram@sil.org>
2019-01-24 07:58:26 -0700

    add a 'wait' in order for the trap to work

--- dockerbuild/run.sh
+++ dockerbuild/run.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
+set -x
 
-trap "apache2ctl stop" SIGINT SIGTERM
+# establish a signal handler to catch the SIGTERM from a 'docker stop'
+# reference: https://medium.com/@gchudnov/trapping-signals-in-docker-containers-7a57fdda7d86
+term_handler() {
+  apache2ctl stop
+  exit 143; # 128 + 15 -- SIGTERM
+}
+trap 'kill ${!}; term_handler' SIGTERM
 
 cd /data
 ./setup-logentries.sh
 
-# Run apache in foreground
-apache2ctl -D FOREGROUND
+apache2ctl start
+
+# endless loop with a wait is needed for the trap to work
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
