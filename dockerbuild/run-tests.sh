b691c89b890401ebb9969998855bead812f1bd83
Matt Henderson <matt_henderson@sil.org>
2021-07-20 10:27:58 -0400

    Ensure the hub is ready before running tests that call it

--- dockerbuild/run-tests.sh
+++ dockerbuild/run-tests.sh
@@ -7,6 +7,8 @@ cd /data
 export COMPOSER_ALLOW_SUPERUSER=1; composer install
 
 ./vendor/bin/phpunit -v tests/
+
+whenavail "ssp-hub.local" 80 30 echo Hub ready
 ./vendor/bin/behat \
     --append-snippets \
     --snippets-for=FeatureContext \
