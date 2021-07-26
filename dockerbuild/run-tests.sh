94649761a68d4d52bccfb9f465fa47e24d1edcdf
Matt Henderson <matt_henderson@sil.org>
2021-07-26 11:40:33 -0400

    Split the tests so that child repos only run the metadata tests

--- dockerbuild/run-tests.sh
+++ dockerbuild/run-tests.sh
@@ -3,18 +3,5 @@
 set -e
 set -x
 
-cd /data
-export COMPOSER_ALLOW_SUPERUSER=1; composer install
-
-./vendor/bin/phpunit -v tests/
-
-whenavail "ssp-hub.local"     80 10 echo Hub ready
-whenavail "ssp-hub-idp.local" 80 10 echo IDP 1 ready
-whenavail "ssp-hub-sp.local"  80 10 echo SP 1 ready
-
-./vendor/bin/behat \
-    --append-snippets \
-    --snippets-for=FeatureContext \
-    --no-interaction \
-    --stop-on-failure #\
-    #--strict
+/data/run-metadata-tests.sh
+/data/run-integration-tests.sh
