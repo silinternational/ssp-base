3a194203bafd64fd46154fc9c8724d079adb2a84
Matt Henderson <matt_henderson@sil.org>
2022-01-14 14:42:59 -0500

    Write PHP file for applying dictionary overrides

---
+++ dockerbuild/apply-dictionaries-overrides.php
@@ -0,0 +1,49 @@
+<?php
+
+if (basename(__DIR__) !== 'overrides') {
+    echo "Only run this in an 'overrides' folder, where the originals are in the parent folder.\n";
+    exit (1);
+}
+
+function assertSuccess($result, $valueIndicatingFailure, $messageIfFailed) {
+    if ($result === $valueIndicatingFailure) {
+        echo $messageIfFailed . PHP_EOL;
+        exit(2);
+    }
+}
+
+foreach (glob('*.json') as $fileName) {
+    echo sprintf(
+        "Merging %s dictionary overrides with the original...\n",
+        $fileName
+    );
+
+    $originalFile = realpath('../' . $fileName);
+    assertSuccess($originalFile, false, 'Failed to find the original file.');
+    $changesFile = realpath($fileName);
+    assertSuccess($changesFile, false, 'Failed to find the file with changes.');
+
+    $originalJson = file_get_contents($originalFile);
+    assertSuccess($originalJson, false, 'Failed to read in the original file.');
+    $changesJson = file_get_contents($changesFile);
+    assertSuccess($changesJson, false, 'Failed to read in the file with changes.');
+
+    $originalData = json_decode($originalJson, true);
+    assertSuccess($originalData, null, 'Failed to decode the original JSON.');
+    $changesData = json_decode($changesJson, true);
+    assertSuccess($changesData, null, 'Failed to decode the changes JSON.');
+
+    $combinedData = array_merge($originalData, $changesData);
+    
+    $combinedJson = json_encode($combinedData, JSON_PRETTY_PRINT);
+    assertSuccess($combinedJson, false, 'Failed to encode the combined JSON.');
+    
+    $fileWriteResult = file_put_contents($originalFile, $combinedJson);
+    assertSuccess(
+        $fileWriteResult,
+        false,
+        'Failed to overwrite the original file with the combined JSON.'
+    );
+
+    echo sprintf("Merged '%s'\n", $fileName);
+}
