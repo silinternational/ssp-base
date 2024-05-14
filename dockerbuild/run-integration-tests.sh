2aafb0668270377f63a8cac38212dae030eca59b
briskt <3172830+briskt@users.noreply.github.com>
2024-05-14 17:13:32 +0800

    fix actions-services.yml

--- dockerbuild/run-integration-tests.sh
+++ dockerbuild/run-integration-tests.sh
@@ -7,8 +7,8 @@ cd /data
 export COMPOSER_ALLOW_SUPERUSER=1; composer install
 
 whenavail "ssp-hub.local"  80 15 echo Hub ready
-whenavail "ssp-idp1.local" 80 15 echo IDP 1 ready
-whenavail "ssp-sp1.local"  80 15 echo SP 1 ready
+whenavail "ssp-idp1.local" 80 5 echo IDP 1 ready
+whenavail "ssp-sp1.local"  80 5 echo SP 1 ready
 
 ./vendor/bin/behat \
     --no-interaction \
