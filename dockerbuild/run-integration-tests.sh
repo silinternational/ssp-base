d2d0837bf6f449ed1a717a47bd533a54dac955f1
briskt <3172830+briskt@users.noreply.github.com>
2024-05-14 16:46:52 +0800

    increase whenavail timouts

--- dockerbuild/run-integration-tests.sh
+++ dockerbuild/run-integration-tests.sh
@@ -6,9 +6,9 @@ set -x
 cd /data
 export COMPOSER_ALLOW_SUPERUSER=1; composer install
 
-whenavail "ssp-hub.local"     80 15 echo Hub ready
-whenavail "ssp-idp1.local" 80 5 echo IDP 1 ready
-whenavail "ssp-sp1.local"  80 5 echo SP 1 ready
+whenavail "ssp-hub.local"  80 15 echo Hub ready
+whenavail "ssp-idp1.local" 80 15 echo IDP 1 ready
+whenavail "ssp-sp1.local"  80 15 echo SP 1 ready
 
 ./vendor/bin/behat \
     --no-interaction \
