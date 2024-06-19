40f6f8769d026d1cbadcf3aa422e8eb2bf5d6e43
briskt <3172830+briskt@users.noreply.github.com>
2024-06-19 16:12:57 +0800

    convert expired.php template to Twig

--- features/bootstrap/ExpiryContext.php
+++ features/bootstrap/ExpiryContext.php
@@ -152,7 +152,7 @@ class ExpiryContext extends FeatureContext
     public function iShouldSeeAnErrorMessage()
     {
         $page = $this->session->getPage();
-        Assert::assertContains('An error occurred', $page->getHtml());
+        Assert::assertContains('We could not understand the expiration date', $page->getHtml());
     }
 
     /**
--- modules/expirychecker/public/expired.php
+++ modules/expirychecker/public/expired.php
@@ -18,21 +18,21 @@ if (empty($stateId)) {
 $state = State::loadState($stateId, 'expirychecker:expired');
 
 if (array_key_exists('changepwd', $_REQUEST)) {
-    
+
     /* Now that they've clicked change-password, skip the splash pages very
      * briefly, to let the user get to the change-password website.  */
     ExpiryDate::skipSplashPagesFor(60); // 60 seconds = 1 minute
-    
+
     // The user has pressed the change-password button.
     $passwordChangeUrl = $state['passwordChangeUrl'];
-    
+
     // Add the original url as a parameter
     if (array_key_exists('saml:RelayState', $state)) {
         $stateId = State::saveState(
             $state,
             'expirychecker:about2expire'
         );
-      
+
         $returnTo = Utilities::getUrlFromRelayState(
             $state['saml:RelayState']
         );
@@ -47,11 +47,11 @@ if (array_key_exists('changepwd', $_REQUEST)) {
 
 $globalConfig = Configuration::getInstance();
 
-$t = new Template($globalConfig, 'expirychecker:expired.php');
+$t = new Template($globalConfig, 'expirychecker:expired');
 $t->data['formTarget'] = Module::getModuleURL('expirychecker/expired.php');
 $t->data['formData'] = ['StateId' => $stateId];
 $t->data['expiresAtTimestamp'] = $state['expiresAtTimestamp'];
 $t->data['accountName'] = $state['accountName'];
-$t->show();
+$t->send();
 
 Logger::info('expirychecker - User has been told that their password has expired.');
