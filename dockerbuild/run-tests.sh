5597a92799e8b3d5491b19015a3cbfab007ef6f8
briskt <3172830+briskt@users.noreply.github.com>
2024-07-15 19:43:24 -0600

    fix the AddIdp2NameId filter

--- dockerbuild/run-tests.sh
+++ dockerbuild/run-tests.sh
@@ -9,5 +9,6 @@ set -e
 /data/run-metadata-tests.sh
 
 ./vendor/bin/phpunit -v tests/AnnouncementTest.php
+./vendor/bin/phpunit -v vendor/simplesamlphp/simplesamlphp/modules/sildisco/tests/
 
 /data/run-integration-tests.sh
