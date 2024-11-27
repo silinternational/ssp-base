d8c79e55e9cb698864b0039c1d00ca21eccb01ce
briskt <3172830+briskt@users.noreply.github.com>
2024-11-27 13:56:36 +0800

    fix config-shim parameters

--- dockerbuild/run.sh
+++ dockerbuild/run.sh
@@ -23,7 +23,11 @@ php -v | head -n 1
 if [[ -z "${APP_ID}" ]]; then
   apache2ctl -k start -D FOREGROUND
 else
-  config-shim -u --strategy $STRATEGY_ID --app $APP_ID --config $CONFIG_ID --env $ENV_ID apache2ctl -k start -D FOREGROUND
+  if [[ -z "${STRATEGY_ID}" ]]; then
+    config-shim --app $APP_ID --config $CONFIG_ID --env $ENV_ID apache2ctl -k start -D FOREGROUND
+  else 
+    config-shim -u --strategy $STRATEGY_ID --app $APP_ID --config $CONFIG_ID --env $ENV_ID apache2ctl -k start -D FOREGROUND
+  fi
 fi
 
 # endless loop with a wait is needed for the trap to work
