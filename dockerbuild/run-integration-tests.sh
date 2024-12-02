88816f3ce4afc08aadb34834f551422749aff076
Matt H. <matt_henderson@sil.org>
2024-12-02 16:00:11 -0500

    Increase timeout when tests are waiting for local IDP to start up

--- dockerbuild/run-integration-tests.sh
+++ dockerbuild/run-integration-tests.sh
@@ -9,7 +9,7 @@ set -e
 cd /data
 
 whenavail "ssp-hub.local"  80 15 echo Hub ready
-whenavail "ssp-idp1.local" 80 5 echo IDP 1 ready
+whenavail "ssp-idp1.local" 80 10 echo IDP 1 ready
 whenavail "ssp-sp1.local"  80 5 echo SP 1 ready
 
 ./vendor/bin/behat \
