696748f0fdbdf027438fbf17c428ab37174ad228
Matt H. <matt_henderson@sil.org>
2025-05-05 14:22:14 -0400

    Rename manager_email to masked_manager_email in MFA view parameters
    This brings it in line with the data names from the state array.

--- modules/mfa/public/prompt-for-mfa.php
+++ modules/mfa/public/prompt-for-mfa.php
@@ -139,7 +139,7 @@ $t->data['mfa_options'] = $mfaOptions;
 $browserJsHash = md5_file(__DIR__ . '/simplewebauthn/browser.js');
 $t->data['browser_js_path'] = '/module.php/mfa/simplewebauthn/browser.js?v=' . $browserJsHash;
 $t->data['remember_me_js_path'] = '/module.php/mfa/remember-me.js';
-$t->data['manager_email'] = $state['maskedManagerEmail'];
+$t->data['masked_manager_email'] = $state['maskedManagerEmail'];
 $t->data['other_options'] = $otherOptions;
 $t->data['idp_name'] = $t->getEntityDisplayName($state['IdPMetadata']);
 $t->data['rememberMePreference'] = filter_input(INPUT_COOKIE, 'remember_me_preference') ?? '';
