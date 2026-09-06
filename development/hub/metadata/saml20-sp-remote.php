33ff6c06b75c7c02993348c82385ee30ab87f072
Devon McCarty <devon_mccarty@sil.org>
2026-09-06 13:58:52 -0400

    Merge and prepare metadata.

--- development/hub/metadata/saml20-sp-remote.php
+++ development/hub/metadata/saml20-sp-remote.php
@@ -15,13 +15,13 @@ $metadata['https://ssp-sp1.local'] = [
         [
             'index' => 1,
             'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-            'Location' => 'https://ssp-sp1.local/module.php/saml/sp/saml2-acs.php/ssp-hub'
+            'Location' => 'https://ssp-sp1.local/module.php/saml/sp/saml2-acs.php/sp1'
         ],
     ],
     'SingleLogoutService' => [
         [
             'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-            'Location' => 'https://ssp-sp1.local/module.php/saml/sp/saml2-logout.php/ssp-hub'
+            'Location' => 'https://ssp-sp1.local/module.php/saml/sp/saml2-logout.php/sp1'
         ],
     ],
     'certData' => 'MIIDzzCCAregAwIBAgIJAPnOHgSgAeNrMA0GCSqGSIb3DQEBCwUAMH4xCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJOQzEPMA0GA1UEBwwGV2F4aGF3MQwwCgYDVQQKDANTSUwxDTALBgNVBAsMBEdUSVMxDjAMBgNVBAMMBVN0ZXZlMSQwIgYJKoZIhvcNAQkBFhVzdGV2ZV9iYWd3ZWxsQHNpbC5vcmcwHhcNMTYxMDE3MTIyNzU2WhcNMjYxMDE3MTIyNzU2WjB+MQswCQYDVQQGEwJVUzELMAkGA1UECAwCTkMxDzANBgNVBAcMBldheGhhdzEMMAoGA1UECgwDU0lMMQ0wCwYDVQQLDARHVElTMQ4wDAYDVQQDDAVTdGV2ZTEkMCIGCSqGSIb3DQEJARYVc3RldmVfYmFnd2VsbEBzaWwub3JnMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0u+mXWS8vUkKjtJcK1hd0iGW2vbTvYosgyDdqClcSzwpbWJg1A1ChuiQIf7S+5bWL2AN4zMoem/JTn7cE9octqU34ZJAyP/cesppA9G53F9gH4XdoPgnWsb8vdWooDDUk+asc7ah/XwKixQNcELPDZkOba5+pqoKGjMxfL7JQ6+P6LB+xItzvLBXU4+onbGPIF6pmZ8S74mt0J62Y6ne40BHx8FdrtBgdk5TFcDedW09rRJrTFpi3hGSUkcjqj84B+oLAb08Z0SHoELMp5Yh7Tg5QZ2c+S8I47tQjV72rNhUYhIyFuImzSg27R7aRJ6Jj6sK4zEg0Ai4VhO4RmgyzwIDAQABo1AwTjAdBgNVHQ4EFgQUgkYcMbT0o8kmxAz2O3+p1lDVj1MwHwYDVR0jBBgwFoAUgkYcMbT0o8kmxAz2O3+p1lDVj1MwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEANgyTgMVRghgL8klqvZvQpfh80XDPTZotJCc8mZJZ98YkNC8jnR2RIUJpah+XrgotlKNDOK3HMNuyKGgYcqcno4PdDXKbqp4yXmywdNbbEHwPWDGqZXULw2az+UVwPUZJcJyJuwJjy3diCJT53N9G0LqXfeEsV0OPQPaB2PWgYNraBd59fckmBTc298HuvsHtxUcoXM53ms2Ck6GygGwH1vCg7qyIRRQFL4DiSlnoS8jxt3IIpZZs9FAl1ejtFBepSne9kEo7lLhAWY1TQqRrRXNHngG/L70ZkZonE9TNK/9xIHuaawqWkV6WLnkhT0DHCOw67GP97MWzceyFw+n9Vg==',
@@ -41,13 +41,13 @@ $metadata['https://ssp-sp2.local'] = [
         [
             'index' => 1,
             'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-            'Location' => 'https://ssp-sp2.local/module.php/saml/sp/saml2-acs.php/ssp-hub'
+            'Location' => 'https://ssp-sp2.local/module.php/saml/sp/saml2-acs.php/sp2'
         ],
     ],
     'SingleLogoutService' => [
         [
             'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-            'Location' => 'https://ssp-sp2.local/module.php/saml/sp/saml2-logout.php/ssp-hub'
+            'Location' => 'https://ssp-sp2.local/module.php/saml/sp/saml2-logout.php/sp2'
         ],
     ],
     'IDPList' => [
@@ -66,13 +66,13 @@ $metadata['https://ssp-sp3.local'] = [
         [
             'index' => 1,
             'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-            'Location' => 'https://ssp-sp3.local/module.php/saml/sp/saml2-acs.php/ssp-hub'
+            'Location' => 'https://ssp-sp3.local/module.php/saml/sp/saml2-acs.php/sp3'
         ],
     ],
     'SingleLogoutService' => [
         [
             'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-            'Location' => 'https://ssp-sp3.local/module.php/saml/sp/saml2-logout.php/ssp-hub'
+            'Location' => 'https://ssp-sp3.local/module.php/saml/sp/saml2-logout.php/sp3'
         ],
     ],
     'IDPList' => [
@@ -92,13 +92,13 @@ $metadata['https://ssp-sp4.local'] = [
         [
             'index' => 1,
             'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-            'Location' => 'https://ssp-sp4.local/module.php/saml/sp/saml2-acs.php/ssp-hub'
+            'Location' => 'https://ssp-sp4.local/module.php/saml/sp/saml2-acs.php/sp4'
         ],
     ],
     'SingleLogoutService' => [
         [
             'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-            'Location' => 'https://ssp-sp4.local/module.php/saml/sp/saml2-logout.php/ssp-hub'
+            'Location' => 'https://ssp-sp4.local/module.php/saml/sp/saml2-logout.php/sp4'
         ],
     ],
     'IDPList' => [
@@ -117,13 +117,13 @@ $metadata['https://ssp-sp5.local'] = [
         [
             'index' => 1,
             'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-            'Location' => 'https://ssp-sp5.local/module.php/saml/sp/saml2-acs.php/ssp-hub'
+            'Location' => 'https://ssp-sp5.local/module.php/saml/sp/saml2-acs.php/sp5'
         ],
     ],
     'SingleLogoutService' => [
         [
             'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-            'Location' => 'https://ssp-sp5.local/module.php/saml/sp/saml2-logout.php/ssp-hub'
+            'Location' => 'https://ssp-sp5.local/module.php/saml/sp/saml2-logout.php/sp5'
         ],
     ],
     'IDPList' => [
---
+++ development/sp-local/metadata/saml20-sp-hosted.php
@@ -0,0 +1,29 @@
+<?php
+
+$metadata['sp1'] = [
+    'host' => 'ssp-sp1.local',
+];
+
+$metadata['sp2'] = [
+    'host' => 'ssp-sp2.local',
+];
+
+$metadata['sp3'] = [
+    'host' => 'ssp-sp3.local',
+];
+
+$metadata['sp4'] = [
+    'host' => 'ssp-sp4.local',
+];
+
+$metadata['sp5'] = [
+    'host' => 'ssp-sp5.local',
+];
+
+$metadata['mfa-idp'] = [
+    'host' => 'pwmanager.local',
+];
+
+$metadata['admin'] = [
+    'host' => '__DEFAULT__',
+];
